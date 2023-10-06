336ea90eef54a92f654bf221253a50c81d12cd62
Samuel Justice <sam@sweetjusticesound.com>
2023-10-06 12:01:01 +0100

    updated PTSL SDK

--- Examples/ProTools/ImportMedia.py
+++ Examples/ProTools/ImportMedia.py
@@ -16,7 +16,7 @@ def send_message_to_sweejhelper(message):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     print("Connecting to the server...")
-    client_socket.connect(('localhost', 65500))  # use the same port number as in your Swift application
+    client_socket.connect(('localhost', 65500))
     
     print("Sending message to the server...")
     client_socket.send(message.encode('utf-8'))
@@ -24,8 +24,7 @@ def send_message_to_sweejhelper(message):
     print("Shutting down write part of the socket...")
     client_socket.shutdown(socket.SHUT_WR)
 
-    # Set a timeout for receiving the response
-    client_socket.setblocking(0)  # non-blocking mode
+    client_socket.setblocking(0)
 
     start_time = time.time()
     response = b""
@@ -33,22 +32,19 @@ def send_message_to_sweejhelper(message):
     while True:
         try:
             print("Attempting to receive data from the server...")
-            chunk = client_socket.recv(4096)  # receive data from the server
+            chunk = client_socket.recv(4096)
             if chunk:
                 print("Received data chunk from the server.")
                 response += chunk
             else:
-                # No more data to receive
                 break
         except socket.error:
-            # No data received, sleep for a bit before retrying
             print("No data received, sleeping for a bit before retrying...")
-            time.sleep(2)  # Add a delay to allow server to finish receiving
+            time.sleep(2)
 
     print("Closing client socket connection...")
     client_socket.close()
 
-    # Parse the response as a JSON object
     if response:
         try:
             response_json = json.loads(response.decode('utf-8'))
@@ -56,34 +52,19 @@ def send_message_to_sweejhelper(message):
         except json.JSONDecodeError:
             print("Did not receive a valid JSON response from the server")
 
-# Generate a unique request id
 request_id = str(uuid.uuid4())
 
-# Arguments to pass to the 'importMedia' function
-# Available argument options:
-# "sessionPath" [type: str, example: "/path/to/session"],
-# "importType" [type: str, options: "Session", "Audio", "Video", "MIDI", "ClipGroups" (default)],
-# "audioOptions" [type: str, options: "LinkToSource", "CopyFromSource", "ConsolidateFromSource", "ForceToTargetSessionFormat" (default)],
-# "audioHandleSize" [type: int, example: 1024],
-# "videoOptions" [type: str, options: "LinkToSource", "CopyFromSource" (default)],
-# "matchOptions" [type: str, options: "None", "MatchTracks", "ImportAsNewTrack" (default)],
-# "playlistOptions" [type: str, options: "ImportReplaceExistingPlaylists", "ImportOverlayNewOnExistingPlaylists", "DoNotImport" (default)],
-# "trackDataPresetPath" [type: str, example: "/path/to/preset"],
-# "clipGain" [type: bool, example: True],
-# "clipsAndMedia" [type: bool, example: True],
-# "volumeAutomation" [type: bool, example: True],
-# "timeCodeMappingOptions" [type: str, options: "MaintainAbsoluteTimeCodeValues", "MaintainRelativeTimeCodeValues", "MapStartTimeCodeTo" (default)],
-# "timeCodeMappingStartTime" [type: str, example: "00:00:00:00"],
-# "adjustSessionStartTimeToMatchSource" [type: bool, example: True],
-# "filesList" [type: list, example: ["/path/to/media/file1", "/path/to/media/file2"]],
-# "audioOperations" [type: str, options: "Add", "Copy", "Convert", "Default" (default)],
-# "destination" [type: str, options: "None", "MainVideoTrack", "NewTrack", "ClipList" (default)],
-# "location" [type: str, options: "None", "SessionStart", "SongStart", "Selection", "Spot" (default)]
 arguments = {
     "sessionPath": "/path/to/session",
     "importType": "Audio",
-    "audioOptions": "ForceToTargetSessionFormat",
-    "audioHandleSize": 1024,
+    "audioData": {
+        "audioOptions": "ForceToTargetSessionFormat",
+        "audioHandleSize": 1024,
+        "audioOperations": "Default",
+        "destination": "ClipList",
+        "location": "Spot",
+        "filesList": ["/path/to/media/file1", "/path/to/media/file2"]
+    },
     "videoOptions": "CopyFromSource",
     "matchOptions": "ImportAsNewTrack",
     "playlistOptions": "DoNotImport",
@@ -94,17 +75,8 @@ arguments = {
     "timeCodeMappingOptions": "MapStartTimeCodeTo",
     "timeCodeMappingStartTime": "00:00:00:00",
     "adjustSessionStartTimeToMatchSource": True,
-    "filesList": ["/path/to/media/file1", "/path/to/media/file2"]
-    "audioOperations": "Default",
-    "destination": "ClipList",
-    "location": "Spot"
 }
 
-# Convert the arguments to a JSON string
 json_arguments = json.dumps(arguments)
-
-# URL encode the JSON arguments
 encoded_arguments = urllib.parse.quote(json_arguments)
-
-# Send a request to import media, including the request id and arguments in the URL
 send_message_to_sweejhelper(f'sweejhelper://proToolsFunction/importMedia/{request_id}?arguments={encoded_arguments}')
